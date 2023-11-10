ad76dc9c7697e91fbbb46fefaac6f4d3cbebf245
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-11-10 12:55:15 -0500

    trying some things out

---
+++ src/app/chip/page.tsx
@@ -0,0 +1,9 @@
+export default function Chip() {
+    return (
+        <div>
+            <svg viewBox="0 0 500 500" width="500" height="500" style={{border: "1px solid red"}}  transform-origin="50% 50%">
+                
+            </svg>
+        </div>
+    )
+}
---
+++ src/app/testpage/testpage.tsx
@@ -0,0 +1,18 @@
+export default function TestPage() {
+    return (
+        <div style={{
+            // border: "1px solid red",
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "#7F7F7F",
+            fontSize: "12rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "rgb(35,103,141)"
+        }}>
+            some text
+        </div>
+    )
+}
