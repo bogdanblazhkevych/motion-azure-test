b072d17ab2a585dabba07734c416abff60086802
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-11-13 17:14:33 -0500

    began working on navbar

--- src/app/navbar/navbar.tsx
+++ src/app/navbar/navbar.tsx
@@ -4,7 +4,31 @@ export default function Navbar(){
 
     return (
         <div className={styles.navbarwrapper}>
+            <div className={styles.leftwrapper}>
+                <div className={styles.companyname}>
+                    MOTION!
+                </div>
+            </div>
 
+            <nav className={styles.navbar}>
+                <ul>
+                    <li>
+                        <a>
+                            Become a Partner
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Join Waitlist
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Talent
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
