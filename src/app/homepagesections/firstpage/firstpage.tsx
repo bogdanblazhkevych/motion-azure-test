4d17da598447c51e944acce247ef77a8fbdee0f5
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-11-14 17:11:55 -0500

    text is responsive

--- src/app/homepagesections/firstpage/firstpage.tsx
+++ src/app/homepagesections/firstpage/firstpage.tsx
@@ -3,13 +3,16 @@ import styles from './styles.module.css'
 export default function FirstPage(){
     return(
         <div className={styles.firstpagewrapper}>
-            <div className={styles.herotextwrapper}>
-                <h1 className={styles.herotitle}>
-                    Success starts with a great product
-                </h1>
-                <p className={styles.heroparagraph}>
-                    We are building something special shaping the future of automation to unleash the next wave of innovation and productivity. One platform Any device.
-                </p>
+            
+            <div className={styles.textcontainer}>
+                <div className={styles.herotextwrapper}>
+                    <h1 className={styles.herotitle}>
+                        Success starts with a great product
+                    </h1>
+                    <p className={styles.heroparagraph}>
+                        We are building something special shaping the future of automation to unleash the next wave of innovation and productivity. One platform Any device.
+                    </p>
+                </div>
             </div>
 
             <div className={styles.svgwrapper}>
