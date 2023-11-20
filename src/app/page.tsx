bb63f691329c737f0c849a32e15097fc5d44de45
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-11-19 21:34:27 -0500

    forgot to commit, starting card thingy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@ import FirstPage from './homepagesections/firstpage/firstpage'
 import Navbar from './components/navbar/navbar'
 import styles from './page.module.css'
 import Slider from './components/slider/page'
+import SecondPage from './homepagesections/secondpage/secondpage'
 
 export default function Home() {
 
   return (
     <main className={styles.main}>
       {/* <Navbar /> */}
-      <FirstPage />
-      <Slider />
-      <FirstPage />
+      {/* <FirstPage />
+      <Slider /> */}
+      <SecondPage />
     </main>
   )
 }
