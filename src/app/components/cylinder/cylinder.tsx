2af574982d9fe16f1f7131676ca073e2c8412ff5
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-11-22 17:40:28 -0500

    spins

--- src/app/components/cylinder/cylinder.tsx
+++ src/app/components/cylinder/cylinder.tsx
@@ -1,17 +1,23 @@
 import styles from './styles.module.css'
+import { RefObject, useRef } from 'react'
 
-export default function Cylinder() {
+type CylinderProps = {
+    cylinderSVGRef: React.RefObject<SVGSVGElement>
+}
 
-
+export default function Cylinder(props: CylinderProps) {
+    const {cylinderSVGRef} = props
     return (
-        <svg className={styles.svg} xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 1000 1000">
-            <defs>
-            </defs>
-            {/* <path className={styles.bigtop} fill="blue" d="M504,116.044c224.945,0,407.3,173.694,407.3,387.956S728.945,891.956,504,891.956,96.7,718.262,96.7,504,279.055,116.044,504,116.044Z" /> */}
-            <ellipse className={styles.bigtop} cx="500" cy="500" rx="404.106" r="400" />
-            <circle id="small1" className={styles.smallcircle} cx="504" cy="162" r="18" />
-            <circle id="small2" className={styles.smallcircle} cx="504.5" cy="845.5" r="18" />
-            <circle id="small3" className={styles.smallcircle} cx="144.5" cy="500" r="18" />
-        </svg>
+        // <div ref={cylinderSVGRef}>
+            <svg ref={cylinderSVGRef} className={styles.svg} xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 1000 1000">
+                <defs>
+                </defs>
+                {/* <path className={styles.bigtop} fill="blue" d="M504,116.044c224.945,0,407.3,173.694,407.3,387.956S728.945,891.956,504,891.956,96.7,718.262,96.7,504,279.055,116.044,504,116.044Z" /> */}
+                <ellipse className={styles.bigtop} cx="500" cy="500" rx="404.106" r="400" />
+                <circle id="small1" className={styles.smallcircle} cx="504" cy="162" r="18" />
+                <circle id="small2" className={styles.smallcircle} cx="504.5" cy="845.5" r="18" />
+                <circle id="small3" className={styles.smallcircle} cx="144.5" cy="500" r="18" />
+            </svg>
+        // </div>
     )
 }
--- src/app/homepagesections/fourthpage/fourthpage.tsx
+++ src/app/homepagesections/fourthpage/fourthpage.tsx
@@ -3,14 +3,29 @@
 import Cylinder from '@/app/components/cylinder/cylinder'
 import styles from './styles.module.css'
 import { useEffect, useRef } from 'react'
+import CardOne from '@/app/components/cardone/cardone'
+import React from 'react'
 
 export default function FourthPage() {
+    const cylinderSVGRef = useRef<SVGSVGElement>(null)
     const cylinderRef = useRef<HTMLDivElement>(null)
     const cylinderWrapperRef = useRef<HTMLDivElement>(null)
     const handleCylinderScroll = () => {
         console.log("scrolling...")
-        console.log("offsetTop", cylinderRef.current?.getBoundingClientRect())
-        console.log("scrolly: ", window.scrollY)
+        console.log("cylinderoffsetTop", cylinderRef.current?.getBoundingClientRect())
+        // console.log("cylinderWrapperRefoffsettop ", cylinderWrapperRef.current?.getBoundingClientRect())
+        if (! cylinderWrapperRef.current?.getBoundingClientRect() || !cylinderRef.current?.getBoundingClientRect() || !cylinderSVGRef.current) return
+        const percentage = (cylinderWrapperRef.current?.getBoundingClientRect().bottom - cylinderRef.current?.getBoundingClientRect().bottom) / (cylinderWrapperRef.current?.getBoundingClientRect().height - cylinderRef.current?.getBoundingClientRect().height)
+        const degree = percentage * 180-90;
+
+        cylinderSVGRef.current.style.transform = `rotateX(60deg) rotate(${degree}deg)`
+
+
+        // console.log("degrees: ", `${degree}deg`)
+        // console.log((cylinderWrapperRef.current?.getBoundingClientRect().bottom - cylinderRef.current?.getBoundingClientRect().bottom) / (cylinderWrapperRef.current?.getBoundingClientRect().height - cylinderRef.current?.getBoundingClientRect().height)) 
+        // console.log("distance: ", (cylinderWrapperRef.current?.getBoundingClientRect().height - cylinderRef.current?.getBoundingClientRect().height))
+        // console.log(cylinderWrapperRef.current?.getBoundingClientRect().y - cylinderRef.current?.getBoundingClientRect().height)
+        // console.log("scrolly: ", window.scrollY)
     }
     useEffect(() => {
         window.addEventListener('scroll', handleCylinderScroll);
@@ -20,12 +35,18 @@ export default function FourthPage() {
         <div className={styles.fourthpagewrapper}>
             <div className={styles.fourthpagecontainer}>
                 <div className={styles.cardswrapper}>
+                <CardOne heading="A catalyst for startup success"
+                                        paragraph="We believe in leveling the playing field and empowering people to use technology for value. That's why we're building something special. A company that will be different from anything else on the market. Motion Platform, a product that will fundamentally change how R&D and Innovation teams work and empower leaders to leverage AI-powered data analytics for successful decision-making and execution."/>
+                <CardOne heading="A catalyst for startup success"
+                                        paragraph="We believe in leveling the playing field and empowering people to use technology for value. That's why we're building something special. A company that will be different from anything else on the market. Motion Platform, a product that will fundamentally change how R&D and Innovation teams work and empower leaders to leverage AI-powered data analytics for successful decision-making and execution."/>
 
+                <CardOne heading="A catalyst for startup success"
+                                        paragraph="We believe in leveling the playing field and empowering people to use technology for value. That's why we're building something special. A company that will be different from anything else on the market. Motion Platform, a product that will fundamentally change how R&D and Innovation teams work and empower leaders to leverage AI-powered data analytics for successful decision-making and execution."/>
                 </div>
 
                 <div className={styles.cylinderwrapper} ref={cylinderWrapperRef}>
                     <div className={styles.cylindercontainer} ref={cylinderRef}>
-                        <Cylinder />
+                        <Cylinder cylinderSVGRef={cylinderSVGRef}/>
                     </div>
                 </div>
             </div>
