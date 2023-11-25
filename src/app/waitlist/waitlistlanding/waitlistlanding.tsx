e2e071efc02795aff8f4413cc603d7f8cc882a90
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-11-25 17:03:46 -0500

    added talent page

---
+++ src/app/waitlist/waitlistlanding/waitlistlanding.tsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.css'
+
+export default function Waitlist() {
+    return(
+        <div className={styles.talentlandingwrapper}>
+            <div className={styles.talentlandingcontainer}>
+                <div>
+                    logo
+                </div>
+
+                <h1>
+                    Motion Early Access Program
+                </h1>
+
+                <p>
+                    Building the future of automation and innovation ecosystem connectedness. 
+                </p>
+            </div>
+        </div>
+    )
+}
