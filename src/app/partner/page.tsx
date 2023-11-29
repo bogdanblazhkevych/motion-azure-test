ffc537d895d4a54b75e91b837d7323e2c3b9ff46
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-11-29 18:46:10 -0500

    added basic form

--- src/app/partner/page.tsx
+++ src/app/partner/page.tsx
@@ -1,10 +1,12 @@
 import FirstPage from "../homepagesections/firstpage/firstpage";
+import FormPage from "./formpage/formpage";
 import PartnerLanding from "./partnerlanding/partnerlanding";
 
 export default function Partner() {
     return (
         <main>
             <PartnerLanding />
+            <FormPage />
         </main>
     )
 }
