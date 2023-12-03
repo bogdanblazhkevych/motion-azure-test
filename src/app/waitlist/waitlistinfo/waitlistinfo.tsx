bfed9bc71b0771e5f7b8c6f2078ca724b1753e8f
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-12-03 11:01:46 -0500

    styled the cards more

--- src/app/waitlist/waitlistinfo/waitlistinfo.tsx
+++ src/app/waitlist/waitlistinfo/waitlistinfo.tsx
@@ -9,54 +9,41 @@ export default function WaitlistInfo() {
             <div className={styles.waitlistinfocontainer}>
                 <div className={styles.descriptioncontiner}>
 
-                    <div className={styles.description}>
-                        <h1>
-                            Sneak peek at pre-release products
-                        </h1>
+                    {waitlistDescription("Sneak peek at pre-release products", "Get exclusive early access to Motion's newest automation platforms and solutions not yet available to the general public. Be the first to take a test drive.", "Experience the latest innovations before anyone else and pilot new technologies coming down the pipeline.", "Preview")}
 
-                        <p>
-                            Get exclusive early access to Motion's newest automation platforms and solutions not yet available to the general public. Be the first to take a test drive.
-                        </p>
+                    {waitlistDescription("Give valuable feedback", "Directly influence Motion's product roadmap by providing feedback on early releases. Help shape the future direction of the offerings.", "Your input directly guides development priorities and the types of new features we build.", "Improve")}
 
-                        {lineSVG()}
-                    </div>
+                    {waitlistDescription("Test out new features", "Experience and experiment with the latest features Motion is developing. See what future functionality is on the roadmap.", "Test cutting-edge capabilities early and validate functionality before public launch.", "Experiment")}
 
-                    <div className={styles.description}>
-                        <h1>
-                            Give valuable feedback
-                        </h1>
-
-                        <p>
-                            Directly influence Motion's product roadmap by providing feedback on early releases. Help shape the future direction of the offerings.
-                        </p>
-
-                        {lineSVG()}
-                    </div>
-
-                    <div className={styles.description}>
-                        <h1>
-                            Test out new features
-                        </h1>
-
-                        <p>
-                            Directly influence Motion's product roadmap by providing feedback on early releases. Help shape the future direction of the offerings.
-                        </p>
-
-                        {lineSVG()}
-                    </div>
-
-                    <div className={styles.description}>
-                        <h1>
-                            Join our private community of innovators
-                        </h1>
-
-                        <p>
-                            Connect with and learn from other cutting-edge companies that are pushing the boundaries in cloud-AI automation.
-                        </p>
-                    </div>
+                    {waitlistDescription("Join our private community of innovators ", "Connect with and learn from other cutting-edge companies that are pushing the boundaries in cloud-AI automation.", "Collaborate with an exclusive network of leaders and share best practices on optimization.", "Connect")}
 
                 </div>
             </div>
+        </div>
+    )
+}
+
+function waitlistDescription(title: string, paragraphOne: string, paragraphTwo: string, heading: string) {
+    return(
+        <div className={styles.description}>
+
+            <h5>
+                {heading}
+            </h5>
+            
+            <h1>
+                {title}
+            </h1>
+
+            <p>
+                {paragraphOne}
+            </p>
+
+            <p>
+                {paragraphTwo}
+            </p>
+
+            {lineSVG()}
         </div>
     )
 }
