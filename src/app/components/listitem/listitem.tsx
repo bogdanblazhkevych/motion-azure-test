e30426ba91d28b85a0a000261007538206a93cce
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-12-04 17:16:47 -0500

    added animations to listitems

--- src/app/components/listitem/listitem.tsx
+++ src/app/components/listitem/listitem.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useEffect, useRef } from 'react';
+import getValueFromScrollPositon from '../utils';
 import styles from './styles.module.css'
 import { IoCheckbox } from "react-icons/io5";
 
@@ -8,9 +12,35 @@ interface ListItemPropsInterface {
 export default function ListItem(props: ListItemPropsInterface) {
     const { text } = props
 
+    const listItemRef = useRef<HTMLLIElement>(null);
+    const listItemContentRef = useRef<HTMLDivElement>(null);
+    
+    useEffect(() => {
+        const listItemContent = listItemContentRef.current
+
+
+        const listWrapperObserver = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    listItemContent?.classList.add(`${styles.listitemshown}`)
+                }
+            })
+        }, { threshold: 1, });
+    
+        if (listItemContent) {
+            listWrapperObserver.observe(listItemContent)
+        }
+
+        return () => {
+          if (listItemContent) {
+            listWrapperObserver.unobserve(listItemContent);
+          }
+        };
+    }, []);
+
     return (
-        <li>
-            <div className={styles.listitem}>
+        <li ref={listItemRef}>
+            <div ref={listItemContentRef} className={styles.listitem}>
                 <div className={styles.listcheck}>
                     <IoCheckbox />
                 </div>
