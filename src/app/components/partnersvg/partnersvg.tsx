35b2751b7181d957d589d00c5d17e9e9578b9028
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-12-13 20:28:44 -0500

    prepped waitlist svg for animations

--- src/app/components/partnersvg/partnersvg.tsx
+++ src/app/components/partnersvg/partnersvg.tsx
@@ -6,50 +6,42 @@ export default function PartnerSVG() {
     return (
 
         <svg className={styles.waitlistsvg} viewBox='0 0 1000 500'>
-            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none"
-                d="M971.499,503.997 C971.499,36.998 504.500,36.998 504.500,36.998 C504.500,36.998 37.501,36.998 37.501,503.997 " />
-            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none"
-                d="M884.685,502.999 C884.685,36.000 504.500,37.996 504.500,37.996 C504.500,37.996 124.315,36.000 124.315,502.999 " />
-            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none"
-                d="M776.916,502.999 C776.916,36.000 504.500,38.994 504.500,38.994 C504.500,38.994 232.084,36.000 232.084,502.999 " />
-            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none"
-                d="M647.194,502.999 C647.194,36.000 503.502,38.994 503.502,38.994 C503.502,38.994 359.810,36.000 359.810,502.999 " />
-            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none"
-                d="M503.814,503.997 L503.814,40.989 L505.810,38.994 " />
-            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none"
-                d="M36.503,502.999 L970.501,502.999 L970.501,502.999 L970.501,499.007 " />
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M971.499,503.997 C971.499,36.998 504.500,36.998 504.500,36.998 C504.500,36.998 37.501,36.998 37.501,503.997 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M884.685,502.999 C884.685,36.000 504.500,37.996 504.500,37.996 C504.500,37.996 124.315,36.000 124.315,502.999 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M845.819,276.971 C748.946,36.713 504.500,37.996 504.500,37.996 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M776.916,502.999 C776.916,36.000 504.500,38.994 504.500,38.994 C504.500,38.994 381.358,37.640 300.660,178.684 C261.331,247.422 232.084,349.981 232.084,502.999 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M504.500,38.994 C504.500,38.994 381.358,37.640 300.660,178.684 C298.475,182.503 296.321,186.426 294.202,190.456 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M647.194,502.999 C647.194,36.000 503.502,38.994 503.502,38.994 C503.502,38.994 359.810,36.000 359.810,502.999 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M573.751,93.347 C538.368,38.267 503.502,38.994 503.502,38.994 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M503.502,38.994 C503.502,38.994 385.677,36.539 363.415,382.832 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M503.814,503.997 L503.814,40.989 L505.810,38.994 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M36.503,502.999 L970.501,502.999 L970.501,502.999 L970.501,499.007 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M44.1000,405.000 L963.000,405.000 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M71.000,309.000 L936.1000,309.000 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M125.1000,214.000 L884.000,214.000 "/>
+            <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M229.1000,118.000 L778.000,118.000 "/>
+            <path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M36.503,502.999 L970.501,502.999 L970.501,502.999 L970.501,499.007 "/>
             <g className={styles.personicon}>
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M678.822,421.813 C678.822,458.539 642.097,458.539 642.097,458.539 C642.097,458.539 605.371,458.539 605.371,421.813 " />
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M678.822,421.599 C678.822,384.873 642.097,384.873 642.097,384.873 C642.097,384.873 605.371,384.873 605.371,421.599 " />
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M642.099,404.642 C642.099,404.642 633.225,404.642 633.225,413.555 C633.225,422.468 642.099,422.468 642.099,422.468 C642.099,422.468 650.973,422.468 650.973,413.555 C650.973,404.642 642.099,404.642 642.099,404.642 ZM624.793,438.561 C624.793,438.561 642.099,410.711 659.404,438.561 " />
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M624.374,118.110 C624.374,154.836 587.648,154.836 587.648,154.836 C587.648,154.836 550.923,154.836 550.923,118.110 "/>
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M624.374,117.896 C624.374,81.170 587.648,81.170 587.648,81.170 C587.648,81.170 550.923,81.170 550.923,117.896 "/>
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M587.651,100.939 C587.651,100.939 578.777,100.939 578.777,109.852 C578.777,118.765 587.651,118.765 587.651,118.765 C587.651,118.765 596.524,118.765 596.524,109.852 C596.524,100.939 587.651,100.939 587.651,100.939 ZM570.345,134.858 C570.345,134.858 587.651,107.008 604.956,134.858 "/>
             </g>
             <g className={styles.personicon}>
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M231.604,212.782 C231.604,249.508 194.878,249.508 194.878,249.508 C194.878,249.508 158.152,249.508 158.152,212.782 " />
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M231.604,212.568 C231.604,175.842 194.878,175.842 194.878,175.842 C194.878,175.842 158.152,175.842 158.152,212.568 " />
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M194.880,195.611 C194.880,195.611 186.006,195.611 186.006,204.524 C186.006,213.437 194.880,213.437 194.880,213.437 C194.880,213.437 203.754,213.437 203.754,204.524 C203.754,195.611 194.880,195.611 194.880,195.611 ZM177.575,229.530 C177.575,229.530 194.880,201.680 212.185,229.530 " />
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M884.182,308.594 C884.182,345.320 847.456,345.320 847.456,345.320 C847.456,345.320 810.731,345.320 810.731,308.594 "/>
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M884.182,309.380 C884.182,272.655 847.456,272.655 847.456,272.655 C847.456,272.655 810.731,272.655 810.731,309.380 "/>
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M847.459,292.424 C847.459,292.424 838.585,292.424 838.585,301.336 C838.585,310.249 847.459,310.249 847.459,310.249 C847.459,310.249 856.332,310.249 856.332,301.336 C856.332,292.424 847.459,292.424 847.459,292.424 ZM830.153,326.343 C830.153,326.343 847.459,298.492 864.764,326.343 "/>
             </g>
             <g className={styles.personicon}>
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M868.651,266.969 C868.651,303.695 831.925,303.695 831.925,303.695 C831.925,303.695 795.199,303.695 795.199,266.969 " />
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M868.651,266.755 C868.651,230.030 831.925,230.030 831.925,230.030 C831.925,230.030 795.199,230.030 795.199,266.755 " />
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M831.927,249.799 C831.927,249.799 823.053,249.799 823.053,258.711 C823.053,267.624 831.927,267.624 831.927,267.624 C831.927,267.624 840.801,267.624 840.801,258.711 C840.801,249.799 831.927,249.799 831.927,249.799 ZM814.622,283.718 C814.622,283.718 831.927,255.867 849.233,283.718 " />
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M327.209,214.110 C327.209,250.836 290.483,250.836 290.483,250.836 C290.483,250.836 253.757,250.836 253.757,214.110 "/>
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M327.209,213.896 C327.209,177.170 290.483,177.170 290.483,177.170 C290.483,177.170 253.757,177.170 253.757,213.896 "/>
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M290.485,196.939 C290.485,196.939 281.611,196.939 281.611,205.852 C281.611,214.765 290.485,214.765 290.485,214.765 C290.485,214.765 299.359,214.765 299.359,205.852 C299.359,196.939 290.485,196.939 290.485,196.939 ZM273.180,230.858 C273.180,230.858 290.485,203.008 307.791,230.858 "/>
             </g>
             <g className={styles.personicon}>
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M396.542,321.157 C396.542,357.883 359.816,357.883 359.816,357.883 C359.816,357.883 323.090,357.883 323.090,321.157 " />
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M396.542,320.943 C396.542,284.217 359.816,284.217 359.816,284.217 C359.816,284.217 323.090,284.217 323.090,320.943 " />
-                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)"
-                    d="M359.818,303.986 C359.818,303.986 350.944,303.986 350.944,312.899 C350.944,321.812 359.818,321.812 359.818,321.812 C359.818,321.812 368.692,321.812 368.692,312.899 C368.692,303.986 359.818,303.986 359.818,303.986 ZM342.513,337.905 C342.513,337.905 359.818,310.055 377.123,337.905 " />
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M396.541,405.110 C396.541,441.836 359.815,441.836 359.815,441.836 C359.815,441.836 323.090,441.836 323.090,405.110 "/>
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M396.541,404.896 C396.541,368.170 359.815,368.170 359.815,368.170 C359.815,368.170 323.090,368.170 323.090,404.896 "/>
+                <path fill-rule="evenodd" stroke="rgb(0, 0, 0)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="rgb(255, 255, 255)" d="M359.817,387.939 C359.817,387.939 350.944,387.939 350.944,396.852 C350.944,405.765 359.817,405.765 359.817,405.765 C359.817,405.765 368.691,405.765 368.691,396.852 C368.691,387.939 359.817,387.939 359.817,387.939 ZM342.512,421.858 C342.512,421.858 359.817,394.008 377.123,421.858 "/>
             </g>
         </svg>
     )
 }
+
