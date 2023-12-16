9965535eaa880046257315c16b4850847f28bd4d
Bogdan Blazhkevych <bblazhkevychh@gmail.com>
2023-12-16 18:32:30 -0500

    page now responsive

--- src/app/homepagesections/sixthpage/sixthpage.tsx
+++ src/app/homepagesections/sixthpage/sixthpage.tsx
@@ -9,13 +9,17 @@ export default function SixthPage() {
     return(
         <div className={styles.sixthpagewrapper}>
             <div className={styles.sixthpagecontainer}>
-                <h1>
-                    Innovation is hard, but it doesnt have to be.
-                </h1>
+                <AnimateChildren>
+                    <h1 className={styles.pagetitle}>
+                        Innovation is hard, but it doesnt have to be.
+                    </h1>
+                </AnimateChildren>
 
                 <div className={styles.maincontentcontainer}>
                     <AnimateChildren>
-                        {cardTwo("analytics", "Real-Time-Analytics", "Harness the power of real-time analytics for swift, data-driven decisions. Monitor and optimize key metrics instantly, staying ahead in a rapidly evolving digital world.")}
+                        <div className={styles.gridcard}>
+                            {cardTwo("analytics", "Real-Time-Analytics", "Harness the power of real-time analytics for swift, data-driven decisions. Monitor and optimize key metrics instantly, staying ahead in a rapidly evolving digital world.")}
+                        </div>
                     </AnimateChildren>
                     <AnimateChildren>
                         <div className={styles.gridsvg}>
@@ -24,7 +28,9 @@ export default function SixthPage() {
                     </AnimateChildren>
 
                     <AnimateChildren>
-                        {cardTwo("Innovation", "Innovation & Development", "Journey from innovative ideas to tangible prototypes. Merge creativity with practical development, leading to groundbreaking products and market-ready solutions.")}
+                        <div className={styles.gridcard}>
+                            {cardTwo("Innovation", "Innovation & Development", "Journey from innovative ideas to tangible prototypes. Merge creativity with practical development, leading to groundbreaking products and market-ready solutions.")}
+                        </div>
                     </AnimateChildren>
                     <AnimateChildren>
                         <div className={styles.gridsvg}>
@@ -33,7 +39,9 @@ export default function SixthPage() {
                     </AnimateChildren>
 
                     <AnimateChildren>
-                        {cardTwo("Resources", "Access Cutting-Edge Tools", "Access the latest technologies and elite talent. Equip yourself with a comprehensive toolbox for success in today's competitive environment, blending tech with expertise.")}
+                        <div className={styles.gridcard}>
+                            {cardTwo("Resources", "Access Cutting-Edge Tools", "Access the latest technologies and elite talent. Equip yourself with a comprehensive toolbox for success in today's competitive environment, blending tech with expertise.")}   
+                        </div>
                     </AnimateChildren>
                     <AnimateChildren>
                         <div className={styles.gridsvg}>
@@ -42,7 +50,9 @@ export default function SixthPage() {
                     </AnimateChildren>
 
                     <AnimateChildren>
-                        {cardTwo("Launch", "Accelerate Market Entry", "Speed your product from concept to market. Streamline the path from validation to commercialization, ensuring rapid, impactful market entry. Embrace the pace for a standout launch.")}
+                        <div className={styles.gridcard}>
+                            {cardTwo("Launch", "Accelerate Market Entry", "Speed your product from concept to market. Streamline the path from validation to commercialization, ensuring rapid, impactful market entry. Embrace the pace for a standout launch.")}
+                        </div>
                     </AnimateChildren>
                     <AnimateChildren>
                         <div className={styles.gridsvg}>
